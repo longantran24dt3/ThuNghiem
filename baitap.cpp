e456428c69a37afd5da80db910442fd40ffaa396
longantran24dt3 <longantrantdla@gmail.com>
2025-03-08 07:28:21 +0700

    keotep

--- baitap.cpp
+++ baitap.cpp
@@ -1,14 +1,6 @@
 #include<iostream>
 using namespace std ;
 int main (){
-    double  s =0;
-    int n;
-    cout<<" nhap so nguyen n"<<endl;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        s=s+1/i;
-
-    }
-    cout<<s;
-    return 0;
+   cout<<" hoan tat";
+   return 0;
 }
